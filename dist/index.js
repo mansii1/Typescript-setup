9ca348a771be0f1da031287998e763970df288e7
mansii1 <mansimangukiya0@gmail.com>
2025-08-02 22:26:52 +0530

    private public

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,23 @@
 "use strict";
+// class User {
+//   email: string;
+//   private name: string; //you can also define like #name to show private
+//   //   readonly city: string = ""; // You can access it, not use it
+//   private readonly city: string = ""; // if you want to access it, add "private"
+//   constructor(email: string, name: string) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
 class User {
-    constructor(email, name) {
-        this.city = "";
+    constructor(name, email, user) {
         this.name = name;
         this.email = email;
+        this.user = user;
+        this.city = "";
     }
 }
-const mansi = new User("mansi@gmail.com", "mansi");
+const mansi = new User("mansi@gmail.com", "mansi", "user");
+// mansi.name = "manso"; // can't use publicaly if you have add private
 // mansi.city = "surat";
 // mansi.city = "delhi";
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,26 @@
+// class User {
+//   email: string;
+//   private name: string; //you can also define like #name to show private
+//   //   readonly city: string = ""; // You can access it, not use it
+//   private readonly city: string = ""; // if you want to access it, add "private"
+
+//   constructor(email: string, name: string) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
   readonly city: string = "";
-  constructor(email: string, name: string) {
-    this.name = name;
-    this.email = email;
-  }
+  constructor(
+    public name: string,
+    public email: string,
+    private user: string
+  ) {}
 }
 
-const mansi = new User("mansi@gmail.com", "mansi");
+const mansi = new User("mansi@gmail.com", "mansi", "user");
+// mansi.name = "manso"; // can't use publicaly if you have add private
+
 // mansi.city = "surat";
 // mansi.city = "delhi";
