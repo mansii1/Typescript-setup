0f621e2fb133e90d83655f69d8b033fbee9995b7
mansii1 <mansimangukiya0@gmail.com>
2025-08-05 22:07:53 +0530

    generics basic

---
+++ src/myGenerics.ts
@@ -0,0 +1,27 @@
+const score: Array<number> = [];
+const names: Array<number> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+  return val;
+}
+
+function identityTwo(val: any): any {
+  return val;
+}
+
+function identityThree<Type>(val: Type): Type {
+  // same as any : but Type it will return same as value type we have passed on parameter like string, boolean ,number etc.
+  return val;
+}
+
+// identityThree("7"); // true, 3
+function identityFour<T>(val: T): T {
+  return val;
+}
+
+interface Bottle {
+  brand: string;
+  type: number;
+}
+
+identityFour<Bottle>({ brand: "ff", type: 9 });
