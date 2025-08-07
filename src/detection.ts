896370b07a6bdc37e15b883d7ad955625d5f97ac
mansii1 <mansimangukiya0@gmail.com>
2025-08-07 23:03:30 +0530

    Union

--- src/detection.ts
+++ src/detection.ts
@@ -55,3 +55,29 @@ function getFood(pet: Fish | Bird) {
     return "Bird food";
   }
 }
+
+// -----------union------------
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rect {
+  kind: "rect";
+  length: number;
+  width: number;
+}
+
+type Shape = Circle | Square;
+
+function getShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  return shape.side * shape.side;
+}
